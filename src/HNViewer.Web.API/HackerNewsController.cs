98241a6a91c7cd8e6207ee2f41337ef867bdf324
heybecker <26857166+heybecker@users.noreply.github.com>
2020-11-05 16:08:07 -0500

    add api project and config

---
+++ src/HNViewer.Web.API/HackerNewsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using HNViewer.HNClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNViewer.Web.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HackerNewsController : ControllerBase
+    {
+        private readonly IHackerNews _hackerNews;
+
+        public HackerNewsController(IHackerNews hackerNews)
+        {
+            _hackerNews = hackerNews;
+        }
+
+        [HttpGet]
+        public IEnumerable<HackerNewsItem> NewStories(int pageSize = 30, int pageNumber = 1, string titleContains = null)
+        {
+            return _hackerNews.GetNewStories(pageSize, pageNumber, titleContains);
+        }
+    }
+}
---
+++ src/HNViewer.Web.API/Startup.cs
@@ -0,0 +1,46 @@
+using HNViewer.HNClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace HNViewer.Web.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+
+            services.AddHackerNewsClient(Configuration);
+            
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
